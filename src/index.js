59e4a279ed703f81d8ecdfa5e9916a4b00236d4d
Duc-Nguyen98 <ducnin1998@gmail.com>
2020-09-14 00:01:25 +0700

    deploy heroku

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const port = 3003;
 const morgan = require('morgan');
 const ejs = require('ejs');
 const router = require('./routers/index');
-const weather = require('openweather-apis');
 app.use(express.urlencoded({ extended: true })); // gọi đến thư viện body-parser
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,  'views'));
 app.use(express.static(__dirname + '/public/'));
 router(app)
 morgan('dev');
+
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
 
